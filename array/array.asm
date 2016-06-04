c27be7d612cce9b5367b0c90759217a88e7793ac
kirill <shaposhnikov.ks@gmail.com>
2016-06-04 21:23:10 +0300

    Fix array lab

--- array/array.asm
+++ array/array.asm
@@ -2,18 +2,15 @@
 
 CalculateCodeOfChar macro code
 local digital,letter,iter
-push cx         		; сохраняем cx
 push ax
 push bx
 
-mov bh,code     		; сохраняем переменнную code в bh
-mov al,bh       		; копируем регистр bh в al
-cmp al,09h      		; сравниваем al с 9
-add al,30h      		; прибавляем 48 к al для получеения кода цифры
+mov bx,code     		; сохраняем переменнную code в bh
+mov ax,bx       		; копируем регистр bh в al
+add ax,30h      		; прибавляем 48 к al для получеения кода цифры
 
 OutChar ax      		; выводим ax
 
-pop cx              	; восстанавливаем cх из стека
 pop ax
 pop bx
 endm
@@ -28,26 +25,25 @@ OutputNymberbydigital macro number, base
 
 	mov ax, 0h
 	mov cx, 0h
-	mov al, number
+	mov ax, number
+    mov bx, base
 
 	iter:
 		inc cx
-		mov bl, base
-		div bl
+        
+        xor dx, dx ; для расширения ax (чтобы можно было произвести деление без division by zero)
+		div bx
 
-		mov dx, ax  ; сохраняем в стек
 		push dx
-	
-		mov ah, 0h
-		cmp al, bl
-	jbe outdigit
-	jmp iter
+		cmp ax, bx
+	 jbe outdigit
+	 jmp iter
 
-	outdigit:
-	CalculateCodeOfChar al
+	 outdigit:
+	 CalculateCodeOfChar ax
 	for:
 		pop dx   ;вытаскиваем число из цикла
-		CalculateCodeOfChar dh
+        CalculateCodeOfChar dx
 	loop for
 
 	pop cx
@@ -68,27 +64,35 @@ endm
 .stack 1000h
 .data
     dischargesInLine dd 01h
-    array db 1000 dup (?)
-    a db 0h
-    b dw 0a07h
+    ;array dw 1000 dup (?)
+    ;array dw 4096, 2567, 4287, 1748, 5548, 133, 4840, 3481, 3547, 2047     ;      1 choice     result  32294
+    ;array dw 962, 2548, 2312, 1297, 2584, 31, 2101, 5325, 4418, 3964        ;      2 choice     result  25542
+    ;array dw 6281, 4465, 5002, 831, 3856, 5590, 1905, 3682, 1024, 4053     ;      3 choice     result  36689
+    ;array dw 4440, 3168, 4100, 1428, 5150, 466, 654, 4602, 5945, 728        ;      4 choice     result  30681
+    ;array dw 4840, 4925, 2939, 5407, 2625, 4053, 4214, 2581, 5318, 717     ;      5 choice     result 37619
+    ;array dw 4972, 3153, 5701, 1721, 4615, 4810, 1511, 4907, 1951, 5167    ;     6 choice     result 38508
+    ;array dw 300, 6220, 3610, 6190, 5993, 3991, 4319, 4785, 5029, 5398     ;      7 choice     result 45835
+    ;array dw 66, 986, 1317, 4654, 1052, 4644, 4145, 1872, 2593, 4181        ;      8 choice     result 25510
+    ;array dw 3604, 3350, 2772, 5894, 5057, 6307, 2795, 716, 911, 175        ;      9 choice     resut 31581
+    array dw 2786, 1964, 543, 5367, 3729, 968, 6188, 5090, 242, 3431         ;      10 choice   result 30308
+    result dw 0h
 .code
 .486
 start:
-     mov ax,@DATA
-     mov ds,ax
-     mov ax, 0h
-
-     mov cx, 1000
-     mov si, 0
-
-     iter:
-         mov array[si], 04h
-         add al, array[si]
-         inc si
-     loop iter
+    mov ax,@DATA
+    mov ds,ax
+    xor ax, ax
+     
+    mov cx, 10
+    mov si, 0
     
-	mov a, al
-    OutputNymberbydigital a, 10
+    iter:
+        add ax, [array + si]
+        add si, 2
+    loop iter
+    
+    mov result, ax
+    OutputNymberbydigital result, 10
    
     exit:
     mov ax,4C00h
